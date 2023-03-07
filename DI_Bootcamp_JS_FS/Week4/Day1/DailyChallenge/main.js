6f758348bc70cd0397796dd63cf556e4663d7840
Previn - OXO <previn@oxo.mu>
2023-03-07 18:50:38 +0400

    added wk4d1

---
+++ DI_Bootcamp_JS_FS/Week4/Day1/DailyChallenge/main.js
@@ -0,0 +1,15 @@
+let str = ""
+
+for(let i = 0; i < 6; i++){
+    console.log(str += "*")
+
+} 
+
+
+let str2 = ""
+
+for(let x = 0; x < 3; x++){
+    for(let y = 0; y < 2; y++){
+        console.log(str2 += "*")
+    }
+}
