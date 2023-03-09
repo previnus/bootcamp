c953c7a6ea8e8f7e4483d255c3438ae60f79dcc9
Previn - OXO <previn@oxo.mu>
2023-03-09 21:00:27 +0400

    course completed

--- DI_Bootcamp_JS_FS/Week4/Day2/main.js
+++ DI_Bootcamp_JS_FS/Week4/Day2/main.js
@@ -127,3 +127,122 @@ let persondata = {
 }
 persondata.eat()
 
+/* 3 Ways of declaring function */
+
+function appelation1() {
+}
+const appelation2 = function () {
+}
+const appelation3 = () => {
+}
+
+//Inside an object
+
+const obj = {
+    age: 45,
+    
+    //type 1
+    func1() {
+        console.log(this.age)
+    },
+    
+    //type 2
+    func2: function () {
+        console.log(this.age)
+    },
+    
+    //type 3
+    func3: () => {
+        console.log(this.age)
+    },
+
+    //type 4 - calling outside function
+    func4: appelation1
+}
+
+obj.func1()
+obj.func2()
+obj.func3()
+
+
+
+
+//DECONSTRUCTION BASICS
+
+//Getting variables from objects
+
+const person = {
+    fname: 'Bob',
+    lname: 'Bobson',
+};
+
+// const fname = person.fname;
+// const lastName = person.lname;
+const { fname, lname: lastName } = person;
+console.log(fname, lastName)
+
+//DECONSTRUCTION FROM ARRAY
+const arr = ['111', '222', '333'];
+const triple1 = arr[0];
+const triple2 = arr[1];
+const triple3 = arr[2];
+console.log(triple1, triple2, triple3)
+const [t1, t2, t3] = arr;
+console.log(t1, t2, t3)
+
+//DECONSTRUCTION FROM BOTH ARRAY & OBJECTS
+const users = [
+    {
+        fname: 'Bob',
+        lname: 'Bobson',
+    },
+    {
+        fname: 'Sarah',
+        lname: 'Sason',
+    },
+    {
+        fname: 'Sponge',
+        lname: 'Ocean',
+    }
+];
+const [, , { lname }] = users;
+//const [_, __, {lname}] = users;
+//const [i, ii, {lname}] = users;
+console.log(lname)
+
+//Spreader
+
+// SPREADING IN ARRAYS
+const original = ['111', '222', '333'];
+const arr1 = [1, 2, 3];
+    arr1.push(original[0])
+    arr1.push(original[1])
+    arr1.push(original[2])
+console.log(arr1);
+
+const arr2 = [1, 2, 3];
+arr2.push(...original);
+console.log(arr2);
+
+const arr3 = [1, 2, 3, ...original];
+console.log(arr3);
+
+const copy = [...original];
+console.log(copy);
+
+//SPREADING IN OBJECTS
+const person = {
+    fname: 'Bob',
+    lname: 'Bobson',
+}
+const clone = {
+    ...person
+}
+console.log(clone);
+
+const imperfectClone = {
+    lname: 'Toulet',
+    ...person,
+    fname: 'Damien',
+}
+console.log(imperfectClone);
