e8c36478f3f23b591593369bd94e139634e45744
Previn - OXO <previn@oxo.mu>
2023-03-24 00:30:33 +0400

    exercise 2 done

--- DI_Bootcamp_JS_FS/Week5/Day4/ExerciseXP/Ex2/main.js
+++ DI_Bootcamp_JS_FS/Week5/Day4/ExerciseXP/Ex2/main.js
@@ -12,9 +12,33 @@
 
 Retrieve the form and console.log it.
 
+*/
+
+let form = document.querySelector('form')
+console.log(form)
+
+/*
+
 Retrieve the inputs by their id and console.log them.
 
+*/
+let fname = document.getElementById('fname')
+let lname = document.getElementById('lname')
+
+console.log(fname, lname)
+
+/*
+
 Retrieve the inputs by their name attribute and console.log them.
+
+*/
+let fnname = document.getElementsByName('fname')
+let lnname = document.getElementsByName('lname')
+
+console.log(fnname)
+console.log(lnname)
+
+/*
 
 When the user submits the form (ie. submit event listener)
 use event.preventDefault(), why ?
@@ -28,3 +52,21 @@ The output should be :
     <li>first name of the user</li>
     <li>last name of the user</li>
 </ul> */
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault() //prevent the form to submit
+    let input = document.querySelectorAll('input[type=text]')
+    console.log(input)
+    let ul = document.querySelector('.usersAnswer')
+
+    for(let i = 0; i < input.length; i++){
+        if(input[i].value == ''){
+            alert(getinputs[i].getAttribute('id') + ' is empty')
+            
+        }else{
+            let newEl = document.createElement('li')
+            newEl.textContent = input[i].value
+            ul.appendChild(newEl)
+        }
+    }
+})
