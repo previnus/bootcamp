8c1ab42ca5c721eb3357d4a8e23273372f712727
Previn - OXO <previn@oxo.mu>
2023-04-04 21:31:16 +0400

    function wrap

--- self_tests/api-test-2.js
+++ self_tests/api-test-2.js
@@ -1,15 +1,17 @@
-const url = 'https://api.metals.live/v1/spot';
-const corsUrl = 'https://corsproxy.io'; // have to make use of a proxy because of cors issues
+const getApi = () => {
+  const url = 'https://api.metals.live/v1/spot';
+  const corsUrl = 'https://corsproxy.io'; // have to make use of a proxy because of cors issues
 
-fetch(corsUrl + '/?' + url)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data[0]["gold"])
+  fetch(corsUrl + '/?' + url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data[0]["gold"])
 
-    for(let i = 0; i < data.length; i++){
-      for(const key in data[i]){
-        console.log(`${key} ${data[i][key]}`)
+      for(let i = 0; i < data.length; i++){
+        for(const key in data[i]){
+          console.log(`${key} ${data[i][key]}`)
+        }
       }
-    }
-  })
-  .catch(error => console.error(error));
+    })
+    .catch(error => console.error(error));
+}
