cd2b386464ea84fd1c9638f00266ebc370442d60
Previn K Appadu <previnus@gmail.com>
2023-04-18 18:30:57 +0400

    added wk7d5 project folder

--- DI_Bootcamp_JS_FS/Week7/Day5/ExerciseXP/Ex1/main.js
+++
@@ -1,8 +0,0 @@
-/* Exercise 1 : Find The Sum
-Instructions
-Create a one line function (ie. an arrow function) that receives two numbers as parameters and returns the sum.
- */
-
-const sum = (x, y) => x + y
-
-console.log(sum(5, 10))
