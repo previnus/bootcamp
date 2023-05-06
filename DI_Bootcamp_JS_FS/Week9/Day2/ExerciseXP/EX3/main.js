a456a0d980c02445b6e4321317fbdec61ba19cef
Previn K Appadu <previnus@gmail.com>
2023-05-06 19:53:51 +0400

    restore code

---
+++ DI_Bootcamp_JS_FS/Week9/Day2/ExerciseXP/EX3/main.js
@@ -0,0 +1,78 @@
+/* Exercise 3 : JSON Mario
+Instructions
+Using this code:
+*/
+
+const marioGame = {
+  detail : "An amazing game!",
+  characters : {
+      mario : {
+        description:"Small and jumpy. Likes princesses.",
+        height: 10,
+        weight: 3,
+        speed: 12,
+      },
+      bowser : {
+        description: "Big and green, Hates princesses.",
+        height: 16,
+        weight: 6,
+        speed: 4,
+      },
+      princessPeach : {
+        description: "Beautiful princess.",
+        height: 12,
+        weight: 2,
+        speed: 2,
+      }
+  },
+
+}
+
+/*
+Convert this JS object into a JSON object. What happens to the nested objects ?
+*/
+
+/* const jsonMarioGame = JSON.stringify(marioGame);
+console.log(jsonMarioGame);
+ */
+//The nested objects are preserved and will keep their respective indices. the nested object would be nested inside each object in the following form:
+
+/* {
+    "detail": "An amazing game!",
+    "characters": {
+      "mario": {
+        "description": "Small and jumpy. Likes princesses.",
+        "height": 10,
+        "weight": 3,
+        "speed": 12
+      },
+      "bowser": {
+        "description": "Big and green, Hates princesses.",
+        "height": 16,
+        "weight": 6,
+        "speed": 4
+      },
+      "princessPeach": {
+        "description": "Beautiful princess.",
+        "height": 12,
+        "weight": 2,
+        "speed": 2
+      }
+    }
+  } */
+  
+
+
+
+/*
+Convert and pretty print this JS object into a JSON object. Hint : Check out the JSON lesson on the platform.
+
+*/
+
+const jsonMarioGame = JSON.stringify(marioGame, null, 2);
+console.log(jsonMarioGame);
+
+/*
+
+
+Use your web inspector to add a breakpoint. Check the values of the JSON object in the debugger. */
